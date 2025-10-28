7feb5170829dfe33cea26006d2af1231385e7e25
Otoniel Oliveira <otonielloliveira@gmail.com>
2025-10-28 11:42:35 -0300

    fix: Ensure creation of storage/app/public directory during deployment

--- deploy.sh
+++ deploy.sh
@@ -68,6 +68,7 @@ mkdir -p storage/framework/cache
 mkdir -p storage/framework/sessions
 mkdir -p storage/framework/views
 mkdir -p storage/logs
+mkdir -p storage/app/public
 mkdir -p bootstrap/cache
 
 # Configurar permissões
